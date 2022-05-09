2d914fa64ecb9bb16b564a72604837c98beecf62
lm106 <>
2022-05-09 20:46:43 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { ListsComponent } from './lists/lists.component';
+import { CreateListComponent } from './create-list/create-list.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsComponent,
+    CreateListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'gestion-app-d5d94',
+    appId: '1:913378518131:web:c5a9805290ef9c6b803606',
+    storageBucket: 'gestion-app-d5d94.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBKHH35tLyyTQGPNjIYXBluT-eDSk7WXt8',
+    authDomain: 'gestion-app-d5d94.firebaseapp.com',
+    messagingSenderId: '913378518131',
+    measurementId: 'G-EK6B0C736S',
+  },
   production: false
 };
 
